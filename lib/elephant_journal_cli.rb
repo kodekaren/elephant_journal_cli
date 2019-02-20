16af97c7395d4e09e29432df5879a7076de930fd
karen ercoli <karenercoli@gmail.com>
2019-02-20 15:19:06 +0000

    first commit

---
+++ lib/elephant_journal_cli.rb
@@ -0,0 +1,6 @@
+require "elephant_journal_cli/version"
+
+module ElephantJournalCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/elephant_journal_cli/version.rb
@@ -0,0 +1,3 @@
+module ElephantJournalCli
+  VERSION = "0.1.0"
+end
