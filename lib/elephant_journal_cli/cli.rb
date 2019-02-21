0b49d0e51cfc263f8ed1ba80649e8b13ca06cb28
karen ercoli <karenercoli@gmail.com>
2019-02-21 17:01:29 +0000

    more structure

--- lib/elephant_journal_cli/cli.rb
+++ lib/elephant_journal_cli/cli.rb
@@ -3,6 +3,29 @@
 class ElephantJournalCli::CLI
 
   def call
-    puts "What's on Elephant Journal Today"
+    puts "Top 3 posts on Elephant Journal Today:"
+    list_posts
+    reader
   end
+
+  def list_posts
+    puts "1. 3 Powerful Practices for Healing the Broken Child inside Us."
+    puts "2. So I met this girl."
+    puts "3. BlaBlaBla."
+  end
+
+  def reader
+    puts "What article do you want to read? If not, type exit."
+    while number != "exit"
+      number = gets.chomp
+      case number
+      when "1"
+      puts "Article 1 text"
+      when "2"
+      puts "Article 2 text"
+      when "3"
+      puts "Article 3 text"
+    end
+  end
+
 end
