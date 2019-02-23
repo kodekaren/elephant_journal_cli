d1409bf77fd554ec4c13ea020aab19415e15bdbd
karen ercoli <karenercoli@gmail.com>
2019-02-23 16:53:39 +0000

    adding post numbers to post class

--- lib/elephant_journal_cli/post.rb
+++ lib/elephant_journal_cli/post.rb
@@ -6,5 +6,21 @@ class ElephantJournalCli::Post
     puts "1. 3 Powerful Practices for Healing the Broken Child inside Us."
     puts "2. So I met this girl."
     puts "3. BlaBlaBla."
+
+    post_1 = self.new
+    post_1.title = "3 Powerful Practices for Healing the Broken Child inside Us."
+    post_1.author = "Name 1"
+    post_1.url = ""
+
+    post_2 = self.new
+    post_2.title = "So I met this girl."
+    post_2.author = "Name 2"
+    post_2.url = ""
+    post_3 = self.new
+    post_3.title = "BlaBlaBla."
+    post_3.author = "Name 3"
+    post_3.url = ""
+
+
   end
 end
