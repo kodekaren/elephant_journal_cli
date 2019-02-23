ad1311010904c7f6436d7ec5655bbd969e3db4c5
karen ercoli <karenercoli@gmail.com>
2019-02-23 16:55:31 +0000

    additional changes to post.rb

--- lib/elephant_journal_cli/post.rb
+++ lib/elephant_journal_cli/post.rb
@@ -10,16 +10,17 @@ class ElephantJournalCli::Post
     post_1 = self.new
     post_1.title = "3 Powerful Practices for Healing the Broken Child inside Us."
     post_1.author = "Name 1"
-    post_1.url = ""
+    post_1.url = "blank"
 
     post_2 = self.new
     post_2.title = "So I met this girl."
     post_2.author = "Name 2"
-    post_2.url = ""
+    post_2.url = "blank"
+
     post_3 = self.new
     post_3.title = "BlaBlaBla."
     post_3.author = "Name 3"
-    post_3.url = ""
+    post_3.url = "blank"
 
 
   end
