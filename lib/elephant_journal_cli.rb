3f99e1e54ed3060f150bd57499d8cba7f29335f3
karen ercoli <karenercoli@gmail.com>
2019-02-27 18:29:57 +0000

    adding dependencies

--- lib/elephant_journal_cli.rb
+++ lib/elephant_journal_cli.rb
@@ -1,3 +1,5 @@
+require "nokogiri"
+
 require_relative "./elephant_journal_cli/version"
 require_relative "./elephant_journal_cli/cli"
 require_relative "./elephant_journal_cli/post"
--- lib/elephant_journal_cli/post.rb
+++ lib/elephant_journal_cli/post.rb
@@ -7,22 +7,23 @@ class ElephantJournalCli::Post
   end
 
   def self.scrape_posts
-    post_1 = self.new
-    post_1.title = "3 Powerful Practices for Healing the Broken Child inside Us."
-    post_1.author = "Name 1"
-    post_1.url = "blank"
+    posts = []
+    # post_1 = self.new
+    # post_1.title = "3 Powerful Practices for Healing the Broken Child inside Us."
+    # post_1.author = "Name 1"
+    # post_1.url = "blank"
+    #
+    # post_2 = self.new
+    # post_2.title = "So I met this girl."
+    # post_2.author = "Name 2"
+    # post_2.url = "blank"
+    #
+    #
+    # post_3 = self.new
+    # post_3.title = "BlaBlaBla."
+    # post_3.author = "Name 3"
+    # post_3.url = "blank"
 
-    post_2 = self.new
-    post_2.title = "So I met this girl."
-    post_2.author = "Name 2"
-    post_2.url = "blank"
-
-
-    post_3 = self.new
-    post_3.title = "BlaBlaBla."
-    post_3.author = "Name 3"
-    post_3.url = "blank"
-
-    [post_1, post_2, post_3]
+    posts
   end
 end
